e672c9f08a6b2575bbf88e30afaa889f1e3b54f6
joo-seongchan <jskjsj601@gmail.com>
2022-05-30 10:37:40 +0900

    js.module-2

--- js/btnEvnet.js
+++ js/btnEvnet.js
@@ -1,11 +1,13 @@
-const boxEl = document.querySelector('.con')
-const on_buttonEl = document.querySelector('.add_color')
-const remove_buttonEl = document.querySelector('.remove_color')
+(function(){
+    const boxEl = document.querySelector('.con')
+    const on_buttonEl = document.querySelector('.add_color')
+    const remove_buttonEl = document.querySelector('.remove_color')
 
 
-const handleOn = () => boxEl.classList.add('on')
-const handleRemove = () => boxEl.classList.remove('on')
+    const handleOn = () => boxEl.classList.add('on')
+    const handleRemove = () => boxEl.classList.remove('on')
 
 
-on_buttonEl.addEventListener('click', handleOn)
-remove_buttonEl.addEventListener('click', handleRemove)
+    on_buttonEl.addEventListener('click', handleOn)
+    remove_buttonEl.addEventListener('click', handleRemove) 
+})()
